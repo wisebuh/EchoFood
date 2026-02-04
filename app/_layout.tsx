79755e05e1a6d3f58d834ceff41ba185dcdab45b
unknown <wisebuh3#gmail.com>
2026-02-04 18:02:04 +0100

    adjusted metro.config file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,27 @@
 import { Stack, SplashScreen } from "expo-router";
-import {useFonts} from "expo-font"
+import { useFonts } from "expo-font";
 import { useEffect } from "react";
-import "./global.css"
+import "./global.css";
 
 export default function RootLayout() {
-  
-
   const [fontsLoaded, error] = useFonts({
-    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
-    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
-    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
-    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
-    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+    "Quicksand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "Quicksand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "Quicksand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "Quicksand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "Quicksand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
   });
 
   useEffect(() => {
-    if(error) throw error;
-    if(fontsLoaded) SplashScreen.hideAsync();
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
   }, [fontsLoaded, error]);
 
-  
-
+  if (!fontsLoaded && !error) return null;
 
   return (
-  <Stack screenOptions={{headerShown:false}}>
-    <Stack.Screen
-    name="(tabs)"/>
-    
-  </Stack>
-);
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
 }
